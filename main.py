07f389292981ab642e68fae7691eceae87b0c8d9
AleksandrsBaranovs <125829016+AleksandrsBaranovs@users.noreply.github.com>
2023-03-18 23:19:30 +0000

    4uzd

--- main.py
+++ main.py
@@ -1,53 +1,44 @@
 # python3
 
+import heapq
+
 def parallel_processing(n, m, data):
     output = []
     threads = [(0, i) for i in range(n)]
+    heapq.heapify(threads)
 
     for i in range(m):
-        task_time = data[i]
-        start_time, thread_idx = threads[0]
+        ti = data[i]
+        start_time, thread_idx = heapq.heappop(threads)
         output.append((thread_idx, start_time))
-        threads[0] = (start_time + task_time, thread_idx)
-
-        j = 0
-        while True:
-            left_child = 2 * j + 1
-            right_child = 2 * j + 2
-
-            if left_child >= n:
-                break
-
-            min_child = left_child
-            if right_child < n and threads[right_child][0] < threads[left_child][0]:
-                min_child = right_child
-
-            if threads[min_child][0] < threads[j][0]:
-                threads[j], threads[min_child] = threads[min_child], threads[j]
-                j = min_child
-            else:
-                break
-
-        next_thread = min(threads)
-        threads[next_thread[1]] = (next_thread[0] + task_time, next_thread[1])
-        output.append((next_thread[1], next_thread[0]))
+        heapq.heappush(threads, (start_time + ti, thread_idx))
 
     return output
 
-
 def print_grid(n, m, result):
+    grid = [[' ' for j in range(m)] for i in range(n)]
+    for i in range(m):
+        thread_idx, start_time = result[i]
+        grid[thread_idx][i] = '*'
     for j in range(m):
-        for i in range(n):
-            print(result[j * n + i][0], result[j * n + i][1])
-
+        print('--', end='')
+    print('-')
+    for i in range(n):
+        for j in range(m):
+            print(f'|{grid[i][j]}', end='')
+        print('|')
+        for j in range(m):
+            print('--', end='')
+        print('-')
 
 def main():
-    w, c = map(int, input().split())
-    data = list(map(int, input().split()))
+    n, m = 3, 6 
+    data = [2, 4, 3, 2, 1, 3] 
 
-    result = parallel_processing(w, c, data)
-    print_grid(w, c, result)
+    result = parallel_processing(n, m, data)
 
+    for i, j in result:
+        print(i,j)
 
 if __name__ == "__main__":
     main()
