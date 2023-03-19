644856cf6389ea73d92c9245e6ad5819235fc766
AleksandrsBaranovs <125829016+AleksandrsBaranovs@users.noreply.github.com>
2023-03-19 00:53:53 +0000

    42421412

--- main.py
+++ main.py
@@ -2,45 +2,17 @@
 
 def parallel_processing(n, m, data):
     output = []
-    threads = [(0, i) for i in range(n)]
-
-    for i in range(m):
-        ti = data[i]
-        start_time, thread_idx = threads[0]
-        output.append((thread_idx, start_time))
-        threads[0] = (start_time+ti, thread_idx)
-        j = 0
-        while True:
-            left_child = 2*j + 1
-            right_child = 2*j + 2
-            if left_child >= n:
-                break
-            min_child = left_child
-            if right_child < n and threads[right_child][0] < threads[left_child][0]:
-                min_child = right_child
-            if threads[min_child][0] < threads[j][0]:
-                threads[j], threads[min_child] = threads[min_child], threads[j]
-                j = min_child
-            else:
-                break
-        next_thread = min(threads)
-        threads[next_thread[1]] = (next_thread[0] + data[i], next_thread[1])
-        output.append((next_thread[1], next_thread[0]))
-
+    finish_time = [0] * n
+    for i, job_time in enumerate(data):
+        min_finish_time = min(finish_time)
+        min_thread = finish_time.index(min_finish_time)
+        output.append((min_thread, min_finish_time))
+        finish_time[min_thread] += job_time
     return output
 
-
-    def print_grid(grid):
-        for j in range(len(grid[0])):
-            for i in range(len(grid)):
-                print(i, j)
-                print(grid[i][j])
-
-
-
 def main():
-    n, m = 3, 6 # Hard-coded input values
-    data = [2, 4, 3, 2, 1, 3] # Hard-coded input values
+    n, m = 3, 6 
+    data = [2, 4, 3, 2, 1, 3] 
 
     result = parallel_processing(n, m, data)
 
